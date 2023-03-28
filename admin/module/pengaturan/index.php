38be0614e454eb97dd2eac6c791b9ad85473bd2e
DESKTOP-79LR5HN\Brianly Hedi R <briannrf1@gmail.com>
2023-03-29 02:15:18 +0700

    up

--- admin/module/pengaturan/index.php
+++ admin/module/pengaturan/index.php
@@ -1,10 +1,10 @@
 <h4>Pengaturan Toko</h4>
 <br>
-<?php if(isset($_GET['success'])){?>
-<div class="alert alert-success">
-	<p>Ubah Data Berhasil !</p>
-</div>
-<?php }?>
+<?php if (isset($_GET['success'])) { ?>
+	<div class="alert alert-success">
+		<p>Ubah Data Berhasil !</p>
+	</div>
+<?php } ?>
 <div class="card">
 	<div class="card-body">
 		<form method="post" action="fungsi/edit/edit.php?pengaturan=ubah">
@@ -12,25 +12,25 @@
 				<div class="col-md 6">
 					<div class="form-group">
 						<label for="">Nama Toko</label>
-						<input class="form-control" name="namatoko" value="<?php echo $toko['nama_toko'];?>"
-									placeholder="Nama Toko">
+						<input class="form-control" name="namatoko" value="<?php echo $toko['nama_toko']; ?>"
+							   placeholder="Nama Toko">
 					</div>
 					<div class="form-group">
 						<label for="">Alamat Toko</label>
-						<input class="form-control" name="alamat" value="<?php echo $toko['alamat_toko'];?>"
-									placeholder="Alamat Toko">
+						<input class="form-control" name="alamat" value="<?php echo $toko['alamat_toko']; ?>"
+							   placeholder="Alamat Toko">
 					</div>
 				</div>
 				<div class="col-md 6">
 					<div class="form-group">
 						<label for="">Kontak (Hp)</label>
-						<input class="form-control" name="kontak" value="<?php echo $toko['tlp'];?>"
-									placeholder="Kontak (Hp)">
+						<input class="form-control" name="kontak" value="<?php echo $toko['tlp']; ?>"
+							   placeholder="Kontak (Hp)">
 					</div>
 					<div class="form-group">
 						<label for="">Nama Pemilik Toko</label>
-						<input class="form-control" name="pemilik" value="<?php echo $toko['nama_pemilik'];?>"
-									placeholder="Nama Pemilik Toko">
+						<input class="form-control" name="pemilik" value="<?php echo $toko['nama_pemilik']; ?>"
+							   placeholder="Nama Pemilik Toko">
 					</div>
 				</div>
 			</div>
